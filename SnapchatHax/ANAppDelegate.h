6fab35ffab8d9a8a7ebfbf09a12ff899ff366156
Alex Nichol <alexnichol@comcast.net>
2013-12-17 16:29:50 -0500

    Initial Commit

---
+++ SnapchatHax/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  SnapchatHax
+//
+//  Created by Alex Nichol on 12/17/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnapchatHax/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnapchatHax
+//
+//  Created by Alex Nichol on 12/17/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANAppDelegate class]));
+    }
+}
